6fd354e51aebeef71a063aa342c96351fcafd349
Shoyon Kumar <shoyonkumar143@gmail.com>
2026-04-20 17:18:32 +0600

    plant data added to the database

--- src/pages/Dashboard/Seller/AddPlant.jsx
+++ src/pages/Dashboard/Seller/AddPlant.jsx
@@ -1,7 +1,62 @@
-import { Helmet } from 'react-helmet-async'
-import AddPlantForm from '../../../components/Form/AddPlantForm'
+import { Helmet } from "react-helmet-async";
+import AddPlantForm from "../../../components/Form/AddPlantForm";
+import { imageUplode } from "../../../api/utils";
+import useAuth from "../../../hooks/useAuth";
+import { useState } from "react";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import toast from "react-hot-toast";
 
 const AddPlant = () => {
+  const { user } = useAuth();
+  const axiosSecure = useAxiosSecure();
+  const [uplodeButtonText, setUplodeButtonText] = useState({
+    name: "Uplode Button",
+  });
+  const [loading, setLoading] = useState(false);
+  // handle form submit
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    const form = e.target;
+    const name = form.name.value;
+    const description = form.description.value;
+    const category = form.category.value;
+    const price = parseFloat(form.price.value);
+    const quantity = parseInt(form.quantity.value);
+    const image = form.image.files[0];
+    const imageUrl = await imageUplode(image);
+
+    // seller info
+    const seller = {
+      name: user?.displayName,
+      image: user?.photoURL,
+      email: user?.email,
+    };
+
+    // Create plant data object
+    const plantData = {
+      name,
+      category,
+      description,
+      price,
+      quantity,
+      image: imageUrl,
+      seller,
+    };
+
+    // console.table(plantData);
+    // save plant in db
+    try {
+      // post request
+      await axiosSecure.post("/plants", plantData);
+      toast.success("Data Added Successfully");
+    } catch (err) {
+      console.log(err);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
     <div>
       <Helmet>
@@ -9,9 +64,14 @@ const AddPlant = () => {
       </Helmet>
 
       {/* Form */}
-      <AddPlantForm />
+      <AddPlantForm
+        handleSubmit={handleSubmit}
+        uplodeButtonText={uplodeButtonText}
+        setUplodeButtonText={setUplodeButtonText}
+        loading={loading}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default AddPlant
+export default AddPlant;
